f94ed42354aaabcde1f55a54beb41315a5138bda
root <root@tony.>
2025-05-13 15:16:23 -0400

    Your commit message

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+const burgers = ["hambuguers, Cheeseburguers."];
+
+let featuredDrink = "Starwberry Milkshake";
+
+function addBurguer() {
+    const newBurger = "Flatburger"
+    burgers.push(newBurger);
+}
+    if (true) {
+    const anothernewBurguer = "Maple Bacon Burguer";
+    burguers.push(anothernewBurguer)
+   
+
+
+function changeFeaturedDrink(){
+    const featuredDrink = "The JavaShake"
+     }
+}
